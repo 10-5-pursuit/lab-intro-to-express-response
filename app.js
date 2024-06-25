032a0d5f3511fba562b46224fe85581fcc3a334d
CSAN7690 <christophersanchez@pursuit.org>
2024-06-25 08:58:43 -0400

    Added 3 more routes = homersimpson + jackdawson + rodtidwell

--- app.js
+++ app.js
@@ -11,6 +11,21 @@ app.get('/terminator', (req, res) => {
     res.send("I'll be back");
 });
 
+// Route for ('/homersimpson')
+app.get('/homersimpson', (req, res) => {
+    res.send("D'Oh!");
+})
+
+// Route for ('/jackdawson')
+app.get('/jackdawson', (req, res) => {
+    res.send("I'm king of the world!")
+})
+
+//Route for ('rodtidwell')
+app.get('/rodtidwell', (req, res) => {
+    res.send("Show me the money!")
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
 })
