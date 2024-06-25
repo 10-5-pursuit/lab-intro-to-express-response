d5b58348536468c6af0b1c36777c5171392e9220
CSAN7690 <christophersanchez@pursuit.org>
2024-06-25 09:06:20 -0400

    Created own array of Office quotes as responses for the  magic 8 ball for organization

---
+++ models/magic8Ball.js
@@ -0,0 +1,21 @@
+// Magic 8 Ball responses with an array of the Office quotes 
+
+const officeQuotes = [
+    "You are Beyoncé, always.",
+    "I wish there was a way for you to know you’re in the good old days before you’ve actually left them.",
+    "You can declare bankruptcy!",
+    "Bears, beets, Battlestar Galactica.",
+    "You’re not superstitious, but you are a little stitious.",
+    "That's what she said!",
+    "You can feel God in this Chili’s tonight.",
+    "Sometimes you'll start a sentence and you won't even know where it's going. You just hope you find it along the way.",
+    "You are running away from your responsibilities. And it feels good.",
+    "You understand nothing.",
+    "You’re an early bird and a night owl. So you're wise and you have worms.",
+    "You talk a lot, so you’ve learned to tune yourself out.",
+    "You are about to do something very bold in this job that you've never done before… try.",
+    "You’re always thinking one step ahead, like a carpenter that makes stairs."
+
+]
+
+module.exports = officeQuotes;
