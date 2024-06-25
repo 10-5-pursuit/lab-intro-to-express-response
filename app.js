a76cceba1290b64f5a0933278020cb923b7abbed
CSAN7690 <christophersanchez@pursuit.org>
2024-06-25 09:12:25 -0400

    Add /magic8Ball route to return a random Office quote in an <h1> tag

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const PORT = 3003;
+const officeQuotes = require('./models/magic8Ball');
 
 // Route for the ('/')
 app.get('/', (req, res) => {
@@ -24,6 +25,13 @@ app.get('/jackdawson', (req, res) => {
 //Route for ('rodtidwell')
 app.get('/rodtidwell', (req, res) => {
     res.send("Show me the money!")
+})
+
+// Route for ('/magic8ball')
+app.get('/magic8Ball', (req, res) => {
+    const randomIndex = Math.floor(Math.random() * officeQuotes.length);
+    const magic8Answer = officeQuotes[randomIndex];
+    res.send(`<h1>${magic8Answer}</h1>`);
 })
 
 app.listen(PORT, () => {
